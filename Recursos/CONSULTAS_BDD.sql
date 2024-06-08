e7832d79fa4726a4f02a44cc324b97ab9891024b
JovirEc <jovirodriguezgye@gmail.com>
2024-06-08 16:20:03 -0500

    Se agregaron las consultas BDD

---
+++ Recursos/CONSULTAS_BDD.sql
@@ -0,0 +1,8 @@
+select * from partido
+where fecha_encuentro = '2024-06-22'
+
+select * from pronostico
+where codigo_usuario = '1723415418'
+
+select * from pronostico
+where codigo_partido = 4
--- Recursos/Datos_partidos.sql
+++ Recursos/Datos_partidos.sql
@@ -5,3 +5,8 @@ values ('032','591','20/06/2024','20:00'),
 ('218','862','22/06/2024','14:30'),
 ('484','388','22/06/2024','19:30'),
 ('840','068','23/06/2024','16:30')
+
+select * from pronostico
+insert into pronostico(codigo_usuario,cod_equipo1,cod_equipo2,gol_equipo1,gol_equipo2,codigo_partido) values ('1723415418','032','591',1,2,4);
+insert into pronostico(codigo_usuario,cod_equipo1,cod_equipo2,gol_equipo1,gol_equipo2,codigo_partido) values ('1091200756','604','152',0,0,5);
+insert into pronostico(codigo_usuario,cod_equipo1,cod_equipo2,gol_equipo1,gol_equipo2,codigo_partido) values ('1719690487','218','862',3,2,6);
