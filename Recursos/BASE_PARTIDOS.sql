f1d5704afe69014d16bdebedff57956204236c89
JovirEc <jovirodriguezgye@gmail.com>
2024-06-08 16:08:36 -0500

    Cambios en base de datos

--- Recursos/BASE_PARTIDOS.sql
+++ Recursos/BASE_PARTIDOS.sql
@@ -56,6 +56,13 @@ insert into equipo (nombre,codigo) values ('Colombia','170');
 insert into equipo (nombre,codigo) values ('Paraguay','600');
 insert into equipo (nombre,codigo) values ('Costa Rica','188');
 
+insert into partido (codigo_equipo1,codigo_equipo2,fecha_encuentro,hora_encuentro)
+values ('032','591','20/06/2024','20:00'),
+('604','152','21/06/2024','18:30'),
+('218','862','22/06/2024','14:30'),
+('484','388','22/06/2024','19:30'),
+('840','068','23/06/2024','16:30')
+
 insert into usuario values ('1723415418','CARLOS PAUL','FLORES MUÑOZ');
 insert into usuario values ('1091200756','CONSTANTE FLORENTINO','BETANCOURT JIMENEZ ');
 insert into usuario values ('1719690487','SMARIA BELEN','BETANCOURT LOVATO');
