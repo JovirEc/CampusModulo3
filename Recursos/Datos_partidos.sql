feafd4386918217122b6e22485dfeda5c036c9ba
JovirEc <jovirodriguezgye@gmail.com>
2024-06-08 16:07:29 -0500

    Datos Partidos

---
+++ Recursos/Datos_partidos.sql
@@ -0,0 +1,7 @@
+select * from partido;
+insert into partido (codigo_equipo1,codigo_equipo2,fecha_encuentro,hora_encuentro)
+values ('032','591','20/06/2024','20:00'),
+('604','152','21/06/2024','18:30'),
+('218','862','22/06/2024','14:30'),
+('484','388','22/06/2024','19:30'),
+('840','068','23/06/2024','16:30')
